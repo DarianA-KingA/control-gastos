d90426c08cf0f6dbdf5e362d156960ea9987c9f1
DarianA-KingA <86999768+DarianA-KingA@users.noreply.github.com>
2023-10-22 18:42:18 -0400

    menu inicial agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
+import { generarId } from './Helpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[presupuesto, setPresupuesto]= useState(0)
+  const [isValidPresupueto,setIsValidPresupuesto]= useState(false)
+  const [modal,setModal]=useState(false)
+  const [animarModal,setAnimarModal]=useState(false)
+  const [gastos,setGastos]=useState([])
 
+
+
+  const handleNuevoGasto = () =>{
+    setModal(true)
+    setTimeout(() => {
+       setAnimarModal(true)
+    }, 500);
+  }
+  const guardarGasto = gasto =>{
+      gasto.id = generarId();
+      setGastos([...gastos,gasto])
+
+      setAnimarModal(false)
+        setTimeout(() => {
+            setModal(false)
+         }, 500);
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          isValidPresupueto={isValidPresupueto}
+          setIsValidPresupuesto ={setIsValidPresupuesto}
+        />
+        {
+          isValidPresupueto&&(
+            <div className=' nuevo-gasto'>
+              <img 
+              src={IconoNuevoGasto} 
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto} 
+              />
+            </div>
+          )
+        }
+        {
+          modal&& 
+          <Modal
+            setModal={setModal}
+            animarModal={animarModal}
+            setAnimarModal={setAnimarModal}
+            guardarGasto={guardarGasto}
+          />
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
